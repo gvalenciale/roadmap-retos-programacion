8548ed7589837e36965a708c7bf8c023ba21a5f5
Soy Tavo <gustavo.valenciale@gmail.com>
2025-10-16 20:29:54 -0500

    Update gvalenciale.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/gvalenciale.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/gvalenciale.py
@@ -1,1 +1,35 @@
-print("hola mundo")
+# https://www.python.org/
+
+
+# comentario en una linea.
+
+'''
+esto tambien es 
+un comentario
+en varias líneas
+
+'''
+
+"""
+esto tambien es 
+un comentario
+en varias líneas
+
+"""
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+var_ent = 1
+var_float = 1.2
+var_bool = True
+var_bool = False
+var_str = "Variable de texto"
+var_str = 'Otra variable de texto'
+
+print("Hola, Python")
+
+print(type(var_ent))
+print(type(var_float))
+print(type(var_bool))
+print(type(var_str))
